9aa0d66115fb4f57c20c11b9315cbd4a903a66ce
Korkii <oryamoryam1@gmail.com>
2025-07-02 16:33:29 +0300

    merge

--- Twink/Twink.cpp
+++ Twink/Twink.cpp
@@ -125,78 +125,76 @@ int main(int argc, char* argv[]) {
 
 		Sleep(SECONDS_IN_HOUR * MILI_MULTIPLIER);
 
-	WSADATA wsaData;
-	int iResult;
+		WSADATA wsaData;
+		int iResult;
 
-	// Initialize Winsock
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		printf("WSAStartup failed: %d\n", iResult);
-		return 1;
-	}
+		// Initialize Winsock
+		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+		if (iResult != 0) {
+			printf("WSAStartup failed: %d\n", iResult);
+			return 1;
+		}
 
 
-	struct addrinfo* result = NULL, * ptr = NULL, hints;
+		struct addrinfo* result = NULL, * ptr = NULL, hints;
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+		hints.ai_flags = AI_PASSIVE;
 
-	// Resolve the local address and port to be used by the server
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
+		// Resolve the local address and port to be used by the server
+		iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+		if (iResult != 0) {
+			printf("getaddrinfo failed: %d\n", iResult);
+			WSACleanup();
+			return 1;
+		}
 
-	SOCKET ListenSocket = INVALID_SOCKET;
+		SOCKET ListenSocket = INVALID_SOCKET;
 
-	// Create a SOCKET for the server to listen for client connections
+		// Create a SOCKET for the server to listen for client connections
 
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 
-	if (ListenSocket == INVALID_SOCKET) {
-		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
+		if (ListenSocket == INVALID_SOCKET) {
+			printf("Error at socket(): %ld\n", WSAGetLastError());
+			freeaddrinfo(result);
+			WSACleanup();
+			return 1;
+		}
 
-	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
-		freeaddrinfo(result);
+		iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			printf("bind failed with error: %d\n", WSAGetLastError());
+			freeaddrinfo(result);
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+			printf("Listen failed with error: %ld\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		SOCKET ClientSocket;
+		ClientSocket = INVALID_SOCKET;
+
+		// Accept a client socket
+		ClientSocket = accept(ListenSocket, NULL, NULL);
+		if (ClientSocket == INVALID_SOCKET) {
+			printf("accept failed: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		// No longer need server socket
 		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-		printf("Listen failed with error: %ld\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	SOCKET ClientSocket;
-	ClientSocket = INVALID_SOCKET;
-
-	// Accept a client socket
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET) {
-		printf("accept failed: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	// No longer need server socket
-	closesocket(ListenSocket);
-
-	Sleep(SECONDS_IN_HOUR * MILI_MULTIPLIER);
 
 		closeStatus = CloseHandle(g_hMutex);
 
