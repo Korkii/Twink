e7f5162eed7e8d53ba0aea59b6b7087f9806f99f
Korkii <oryamoryam1@gmail.com>
2025-07-02 14:08:54 +0300

    Add exception classes

---
+++ Twink.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "MyException.h"
+
+
+class AlreadyRunningException : public MyException {
+public:
+	AlreadyRunningException();
+};
+
+class CreateMutexException : public MyException {
+public:
+	CreateMutexException();
+};
+
+class CloseMutexException : public MyException {
+public:
+	CloseMutexException();
+};
+
+class PopupException : public MyException {
+public:
+	PopupException();
+};
+
+class OpenRegistryException : public MyException {
+public:
+	OpenRegistryException();
+};
+
+class SetRegistryException : public MyException {
+public:
+	SetRegistryException();
+};
+
+class GetModuleException : public MyException {
+public:
+	GetModuleException();
+};
--- Twink/Twink.cpp
+++ Twink/Twink.cpp
@@ -1,117 +1,108 @@
-// Twink.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <windows.h>
 #include <winuser.h>
-#include <winreg.h>
-#include <libloaderapi.h>
-#include <winbase.h>
-#include <synchapi.h>
-#include <string.h>
-
+#include "Twink.h"
 
 const int SECONDS_IN_HOUR = 3600;
 const int MILI_MULTIPLIER = 1000;
 const int MAX_PATH_SIZE = 260;
-const char AUTORUNS[46] = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+const char* AUTORUNS = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
 const char* MUTEXNAME = "visual_studio_good";
-const LPCSTR WELCOME_WINDOW_TITLE = "Management program is up (  it was developed in visual studio happily )";
-const LPCSTR WELCOME_WINDOW_TEXT = "Visual Studio made me sad";
+LPCSTR WELCOME_WINDOW_TITLE = "Management program is up (  it was developed in visual studio happily )";
+LPCSTR WELCOME_WINDOW_TEXT = "Visual Studio made me sad";
 const char* AUTORUN_VALUE_NAME = "Technician";
 
 HANDLE g_hMutex;
 
+AlreadyRunningException::AlreadyRunningException() : MyException{ "Already running" } {};
+CreateMutexException::CreateMutexException() : MyException{ "Create Mutex" } {};
+CloseMutexException::CloseMutexException() : MyException{ "Close Mutex" } {};
+PopupException::PopupException() : MyException{ "Popup" } {};
+OpenRegistryException::OpenRegistryException() : MyException{ "Open Registry" } {};
+SetRegistryException::SetRegistryException() : MyException{ "Set Registry" } {};
+GetModuleException::GetModuleException() : MyException{ "Get Module" } {};
+
 enum class StatusCode {
-	SUCCESS,
-	ALREADY_OPEN,
-	CREATE_MUTEX_FAILED,
-	CLOSE_MUTEX_FAILED,
-	POPUP_FAILED,
-	REGOPEN_FAILED,
-	REGSET_FAILED,
-	GET_MODULE_FAILED,
+	STATUS_SUCCESS,
+	STATUS_ERROR,
 };
 
 
-/*
-@brief Handles all error types
 
-@param errorCode Error code to take care of
-@return errorcode
-*/
-int handleErrors(StatusCode errorCode) {
-	switch (errorCode) {
-	case StatusCode::ALREADY_OPEN:
-		std::cerr << "Already open" << std::endl;
-		return static_cast<int>(errorCode);
-	default:
-		std::cerr << GetLastError() << std::endl;
-		return static_cast<int>(errorCode);
+void createMutexStage() {
+	g_hMutex = OpenMutexA(SYNCHRONIZE, FALSE, MUTEXNAME);
+	if (g_hMutex != NULL) {
+		throw AlreadyRunningException();
+	}
+	g_hMutex = CreateMutexA(
+		NULL,
+		TRUE,
+		MUTEXNAME);
+
+	if (g_hMutex == NULL) {
+		throw CreateMutexException();
 	}
 }
 
+void userMessageStage() {
+	int msgboxID = MessageBox(
+		NULL,
+		WELCOME_WINDOW_TITLE,
+		WELCOME_WINDOW_TEXT,
+		MB_ICONINFORMATION
+	);
+	if (msgboxID == NULL) {
+		throw PopupException();
+	}
+}
 
-int main(int argc, char* argv[])
-{
+std::string getFilePath() {
+	CHAR my_str[MAX_PATH_SIZE];
+	LPSTR CurrentPath = my_str;
+	DWORD GetModuleStatus = GetModuleFileNameA(NULL, CurrentPath, MAX_PATH_SIZE);
+	std::string new_str = my_str;
+	new_str = "\"" + new_str + "\"";
+
+	if (GetModuleStatus == NULL) {
+		throw GetModuleException();
+	}
+
+	return new_str;
+}
+
+void SetAutoRun(std::string filePath) {
+	HKEY TargetKey;
+	LSTATUS OpenStat = RegOpenKeyA(HKEY_CURRENT_USER, AUTORUNS, &TargetKey);
+	if (OpenStat != ERROR_SUCCESS) {
+		throw OpenRegistryException();
+	}
+
+	LSTATUS SetStat = RegSetValueExA(TargetKey, AUTORUN_VALUE_NAME, 0, REG_SZ, (BYTE*)filePath.c_str(), MAX_PATH_SIZE);
+	if (SetStat != ERROR_SUCCESS) {
+		throw SetRegistryException();
+	}
+}
+
+int main(int argc, char* argv[]) {
 	try {
-		g_hMutex = OpenMutexA(SYNCHRONIZE, FALSE, MUTEXNAME);
-		if (g_hMutex != NULL) {
-			throw StatusCode::ALREADY_OPEN;
-		}
-		g_hMutex = CreateMutexA(
-			NULL,
-			TRUE,
-			MUTEXNAME);
+		createMutexStage();
+		userMessageStage();
 
-		if (g_hMutex == NULL) {
-			throw StatusCode::CREATE_MUTEX_FAILED;
-		}
-
-		int msgboxID = MessageBox(
-			NULL,
-			WELCOME_WINDOW_TITLE,
-			WELCOME_WINDOW_TEXT,
-			MB_ICONINFORMATION
-		);
-		if (msgboxID == NULL) {
-			throw StatusCode::POPUP_FAILED;
-		}
-
-		HKEY TargetKey;
-		LSTATUS OpenStat = RegOpenKeyA(HKEY_CURRENT_USER, AUTORUNS, &TargetKey);
-		if (OpenStat != ERROR_SUCCESS) {
-			throw StatusCode::REGOPEN_FAILED;
-		}
-
-		CHAR my_str[MAX_PATH_SIZE];
-		LPSTR CurrentPath = my_str;
-		// std::string to LPSTR
-		DWORD GetModuleStatus = GetModuleFileNameA(NULL, CurrentPath, MAX_PATH_SIZE);
-		std::string new_str = my_str;
-		new_str = "\"" + new_str + "\"";
-
-		if (GetModuleStatus == NULL) {
-			throw StatusCode::GET_MODULE_FAILED;
-		}
-		
-		
-		LSTATUS SetStat = RegSetValueExA(TargetKey, AUTORUN_VALUE_NAME, 0, REG_SZ, (BYTE*)new_str.c_str(), MAX_PATH_SIZE);
-		if (SetStat != ERROR_SUCCESS) {
-			throw StatusCode::REGSET_FAILED;
-		}
+		std::string filePath = getFilePath();
+		SetAutoRun(filePath);
 
 		Sleep(SECONDS_IN_HOUR * MILI_MULTIPLIER);
 
 		BOOL CloseStatus = CloseHandle(g_hMutex);
 
 		if (CloseStatus == NULL) {
-			throw StatusCode::CLOSE_MUTEX_FAILED;
+			throw CloseMutexException();
 		}
 
-		return static_cast<int>(StatusCode::SUCCESS);
+		return static_cast<int>(StatusCode::STATUS_SUCCESS);
 	}
-	catch (StatusCode errorCode) {
-		return handleErrors(errorCode);
+	catch (MyException exception) {
+		std::cerr << "An exception occurred (" << exception.getError() << ")\n";
+		return static_cast<int>(StatusCode::STATUS_ERROR);
 	}
 }
